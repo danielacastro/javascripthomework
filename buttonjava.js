f9b0dcf6c3aa04590b7634ee3073b00ae2dfd1e7
Daniela Castro <daniela.castro@journalism.cuny.edu>
2016-02-29 18:08:32 -0500

    uploading the branch

---
+++ buttonjava.js
@@ -0,0 +1,32 @@
+function addNum(){
+    var number1 = document.getElementById("number1");
+    var number2 = document.getElementById("number2");
+    var addition = number1 + number2;
+    alert ('addition');
+
+}
+
+function subsNum(){
+    var number1 = document.getElementById("number1");
+    var number2 = document.getElementById("number2");
+    var substraction = number1 - number2;
+    alert("if you substract " + number1 + "to " + number2 + "you get" + substraction);
+
+}
+
+function multNum(){
+    var number1 = document.getElementById("number1");
+    var number2 = document.getElementById("number2");
+    var multiplication = number1 * number2;
+    alert ('multiplication');
+
+  }
+
+function divitNum(){
+    var number1 = document.getElementById("number1");
+    var number2 = document.getElementById("number2");
+    var division = number1 / number2;
+    alert ('division');
+
+ }
+
